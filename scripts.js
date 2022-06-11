324cecf79509a34f82bd77d427a2a8121d78a521
OthmanAdi <adiatwork@houtmail.com>
2022-06-11 11:22:38 +0200

    complete code with Animejs and ParticleJs library

---
+++ scripts.js
@@ -0,0 +1,25 @@
+let voteUpButton = document.querySelector(".voteUp");
+let voteDownButton = document.querySelector(".voteDown");
+let H2votingDisplay = document.querySelector(".votingDisplay");
+
+let theVote = 0;
+
+showVote(); //rufe die function zu arbeit 🏋🏼
+
+voteUpButton.addEventListener("click", () => {
+    theVote++;
+    showVote();
+})
+
+voteDownButton.addEventListener("click", () => {
+    theVote--;
+    showVote();
+})
+
+function showVote() {
+    if (theVote < 0) {
+        alert("You Cant go Below 0")
+    } else {
+        H2votingDisplay.innerHTML = theVote;
+    }
+}
